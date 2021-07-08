2ee00a31092486bbdf525ca4232a16b9b98bdba9
rajthilakam <rajthilakam@gmail.com>
2021-07-07 18:36:11 -0700

    Single Card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import { AppComponent } from './app.component';
 import { GrocerylistComponent } from './grocerylist/grocerylist.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 
 @NgModule({
@@ -18,6 +23,9 @@ import { MatCardModule } from '@angular/material/card';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatCardModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/grocerylist/grocerylist.component.ts
+++ src/app/grocerylist/grocerylist.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import {ViewEncapsulation } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-grocerylist',
   templateUrl: './grocerylist.component.html',
-  styleUrls: ['./grocerylist.component.css']
+  styleUrls: ['./grocerylist.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class GrocerylistComponent implements OnInit {
+
+  date = new Date();
 
   constructor() { }
 
